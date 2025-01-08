2eb545a3a3b95a5bd5e380145f70a894b533b1cd
Tu Vu <vuanhtu52@gmail.com>
2025-01-08 23:32:12 +1030

    Complete MensPage

--- src/pages/MensPage.jsx
+++ src/pages/MensPage.jsx
@@ -1,5 +1,20 @@
+import { useContext } from "react";
+import ShopContext from "../context/ShopContext";
+import ItemCard from "../components/ItemCard";
+
 const MensPage = () => {
-    return <div>Men's clothing page</div>;
+    const { products } = useContext(ShopContext);
+
+    return <div>
+        <h2 className="text-center text-xl font-bold p-4">Men's Clothing</h2>
+        <div className="grid gap-8 justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-4">
+            {
+                products
+                    .filter(product => product.category === "men's clothing")
+                    .map(product => <ItemCard key={product.id} item={product} />)
+            }
+        </div>
+    </div>;
 };
 
 export default MensPage;
