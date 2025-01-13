b7570c003e72648df210c547c704a0a3a87b33ad
Tu Vu <vuanhtu52@gmail.com>
2025-01-13 23:27:26 +1030

    Fix logic for item cards to refresh quantity after returning to the page

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,9 +6,21 @@ import ShopContext from "../context/ShopContext";
 
 const ItemCard = ({ item }) => {
     const {cartItems, addToCart, removeOneFromCart} = useContext(ShopContext);
+    const [currentQuantity, setCurrentQuantity] = useState(0);
+
+    const handleClickIncrement = () => {
+        setCurrentQuantity(prev => prev + 1);
+        addToCart(item.id);
+    } 
+
+    const handleClickDecrement = () => {
+        setCurrentQuantity(prev => prev - 1);
+        removeOneFromCart(item.id);
+    }
 
     return (
         <div className="flex flex-col min-w-60 w-60 gap-2 border border-gray-300 p-2 rounded-2xl">
+            {/* Image of the item */}
             <Link
                 to={`product/${item.id}`}
             >
@@ -18,29 +30,35 @@ const ItemCard = ({ item }) => {
                     className="object-contain w-full min-h-64 h-64 p-4"
                 />
             </Link>
+
+            {/* Title of the item */}
             <Link
                 to={`product/${item.id}`}
                 className="hover:text-[#fb923c]"
             >{item["title"]}</Link>
+
+            {/* Price */}
             <div className="font-bold text-lg">${item.price}</div>
+
+            {/* Buttons */}
             {
-                !cartItems.hasOwnProperty(item.id) ?
+                currentQuantity === 0 ?
                     <button
                         className="self-start bg-amber-300 p-2 rounded-xl text-sm"
-                        onClick={() => addToCart(item.id)}
+                        onClick={() => handleClickIncrement()}
                     >
                         Add to cart
                     </button> :
                     <div className="self-start flex justify-center items-center border-2 border-amber-300 rounded-xl p-1">
                         {
-                            cartItems[item.id] > 1 ?
-                            <button className="w-5 h-5 flex justify-center items-center" onClick={() => removeOneFromCart(item.id)}>-</button> :
-                            <button className="w-5 h-5" onClick={() => removeOneFromCart(item.id)}>
+                            currentQuantity > 1 ?
+                            <button className="w-5 h-5 flex justify-center items-center" onClick={() => handleClickDecrement()}>-</button> :
+                            <button className="w-5 h-5" onClick={() => handleClickDecrement()}>
                                 <img src={BinIcon} />
                             </button>
                         }
-                        <div className="w-20 text-center">{cartItems[item.id]}</div>
-                        <button className="w-5 h-5 flex justify-center items-center" onClick={() => addToCart(item.id)}>+</button>
+                        <div className="w-20 text-center">{currentQuantity}</div>
+                        <button className="w-5 h-5 flex justify-center items-center" onClick={() => handleClickIncrement()}>+</button>
                     </div>
             }
         </div>
